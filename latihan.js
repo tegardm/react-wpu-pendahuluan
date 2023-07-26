7660b0ade84e7cfa50d87c87ab7c7e18243fe792
Tegar Deyustian <tegardm@gmail.com>
2023-07-26 20:51:27 +0700

    first commit'

---
+++ latihan.js
@@ -0,0 +1,15 @@
+const divId = document.getElementById('app');
+const el = document.createElement('h1');
+el.textContent = 'Belajar React Bareng WPU🚀';
+
+divId.appendChild(el);
+
+function x (a,b) {
+ return a +b;
+}
+
+x(3,5);
+
+const x = () => {
+    return 3+6;
+}
